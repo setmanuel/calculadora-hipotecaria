dc7f10f50af6b436b610c77a9296e35944a93b3c
setmanuel <info@softevolutions.es>
2017-12-15 04:30:15 -0800

    render de cuotas

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            capital: 0,
-            interes: 0,
-            periodo: 0,
-            tipo: 'm',
+            capital: 100000,
+            interes: 1,
+            periodo: 10,
+            tipo: 'a',
+            message: "",
             cuadroAmortizacion: []
         }
 
@@ -27,6 +28,8 @@ class App extends Component {
     formSubmit(event) {
         let url = "http://softevolutions.es/setapp/services/calculadora-hipotecaria/services/calculadora-hipotecaria-fetch.php";
 
+        this.setState({message: "Obteniendo cuadro de amortización"});
+
         fetch(url, {
             method: 'POST',
             mode: 'cors',
@@ -38,7 +41,7 @@ class App extends Component {
         }).then(function(cuadro) {
             return cuadro.json();
         }).then((cuadro) => {
-            this.setState({cuadroAmortizacion: cuadro.response});
+            this.setState({cuadroAmortizacion: cuadro.response.data, message: cuadro.response.message});
         });
 
         event.preventDefault();
@@ -46,40 +49,52 @@ class App extends Component {
 
     render() {
 
-        let cuotas = "";
+        let cuotas = this.state.cuadroAmortizacion.map((linea, i) => {
+            return (<div key={i} className="cuota" title={'Amortizado el ' + (
+                (i + 1) * 100 / this.state.cuadroAmortizacion.length).toFixed(2) + "% => " + (
+                this.state.capital - linea.capital_pendiente).toFixed(2)}>
+                <div>Cuota</div>
+                <div>{i + 1}</div>
+                <div>{linea.capital}</div>
+                <div>{linea.intereses}</div>
+                <div>{linea.cuota}</div>
+                <div>{linea.capital_pendiente}</div>
+            </div>);
+        });
 
-        if(this.state.cuadroAmortizacion.data) {
-            cuotas = this.state.cuadroAmortizacion.data.map((linea, i) => <h6 key={i}>{linea.cuota}</h6>);
+        let cabecera = "";
+
+        if (this.state.cuadroAmortizacion.length > 0) {
+            cabecera = <div className="cuota font-weight-bold my-2">
+                <div></div>
+                <div></div>
+                <div>Captial</div>
+                <div>Intereses</div>
+                <div>Cuota</div>
+                <div>Capital Pendiente</div>
+            </div>;
         }
 
         return (<div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <h1 className="App-title">Calculadora Hipotecaria</h1>
+                <h6>{this.state.message}</h6>
             </header>
 
             <section className="solicitud">
                 <form onSubmit={this.formSubmit}>
                     <div className="form-group">
-                        <label>
-                            Capital:
-                        </label>
-                        <input type="text" className="form-control" name="capital" value={this.state.capital} onChange={this.valueTextChange}/>
+                        <input type="text" className="form-control" name="capital" placeholder="capital" value={this.state.capital} onChange={this.valueTextChange}/>
                     </div>
 
                     <div className="form-group">
-                        <label>
-                            Interés:
-                        </label>
-                        <input type="text" className="form-control" name="interes" value={this.state.interes} onChange={this.valueTextChange}/>
+                        <input type="text" className="form-control" name="interes" placeholder="interés" value={this.state.interes} onChange={this.valueTextChange}/>
                     </div>
 
                     <div className="form-group">
-                        <label>
-                            Periodo:
-                        </label>
                         <div className="form-inline">
-                            <input type="text" className="form-control" name="periodo" value={this.state.periodo} onChange={this.valueTextChange}/>
+                            <input type="text" className="form-control" name="periodo" placeholder="periodo" value={this.state.periodo} onChange={this.valueTextChange}/>
 
                             <select name="tipo">
                                 <option value="m">Meses</option>
@@ -88,11 +103,12 @@ class App extends Component {
                         </div>
                     </div>
 
-                    <input type="submit" value="Ver Cuadro" title="ver el cuadro de amortización"/>
+                    <button className="btn btn-primary" title="ver el cuadro de amortización">Mostrar Cuadro</button>
                 </form>
             </section>
 
             <section>
+                {cabecera}
                 {cuotas}
             </section>
 
